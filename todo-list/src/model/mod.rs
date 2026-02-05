09caede5803fe2c2939388a9c984fdb46708294c
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2026-02-05 14:53:41 +0100

    fix: parse sqlite timestamps properly

--- todo-list/src/model/mod.rs
+++ todo-list/src/model/mod.rs
@@ -5,14 +5,18 @@ pub use item::{Item, ItemId};
 pub use todo_list::{TodoList, TodoListId};
 
 use anyhow::{Context as _, Result};
-use time::{UtcDateTime, format_description::well_known::Iso8601};
+use time::{UtcDateTime, format_description::StaticFormatDescription, macros::format_description};
 use turso::Row;
+
+static SQLITE_TIMESTAMP_FORMAT: StaticFormatDescription =
+    format_description!("[year]-[month]-[day] [hour]:[minute]:[second]");
 
 fn parse_date(row: &Row, column: usize) -> Result<UtcDateTime> {
     let date = row
         .get::<String>(column)
         .context("parse_date: getting value from row")?;
-    UtcDateTime::parse(&date, &Iso8601::DATE_TIME).context("parse_date: parsing the date")
+    UtcDateTime::parse(&date, SQLITE_TIMESTAMP_FORMAT)
+        .context(format!("parse_date: parsing the date ({date:?})"))
 }
 
 fn parse_id<Id>(row: &Row, column: usize) -> Result<Id>
