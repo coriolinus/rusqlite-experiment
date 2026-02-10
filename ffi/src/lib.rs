4518b03daaba56424f908c04544d0b7a27bfb036
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2026-02-10 09:57:15 +0100

    feat: export `apply_schema` over wasm

--- ffi/src/lib.rs
+++ ffi/src/lib.rs
@@ -1,5 +1,14 @@
 mod database;
 mod error;
 
+use wasm_bindgen::prelude::*;
+
 pub use database::Database;
 pub use error::{Context, Error, Result};
+
+#[wasm_bindgen]
+pub async fn apply_schema(database: &mut Database) -> Result<()> {
+    todo_list::apply_schema(&mut database.connection)
+        .await
+        .map_err(Into::into)
+}
