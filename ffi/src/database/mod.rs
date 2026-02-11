d5be6ee5ae3a75283cd77e0c08a15dd6d71a30ec
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2026-02-11 14:04:50 +0100

    feat(ffi): adjust to rusqlite not turso

--- ffi/src/database/mod.rs
+++ ffi/src/database/mod.rs
@@ -1,5 +1,5 @@
 use crate::{Context as _, Result};
-use turso::Connection;
+use rusqlite::Connection;
 use wasm_bindgen::prelude::*;
 
 /// A connection to a Turso database
@@ -12,11 +12,7 @@ pub struct Database {
 impl Database {
     /// Connect to a database
     pub async fn connect(name: &str) -> Result<Self> {
-        let database = turso::Builder::new_local(name)
-            .build()
-            .await
-            .context("building database")?;
-        let connection = database.connect().context("connecting to database")?;
+        let connection = rusqlite::Connection::open(name).context("opening database connection")?;
         Ok(Self { connection })
     }
 }
