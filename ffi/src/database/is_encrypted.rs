9ca80c25e9526a0935998eab4e01808fdbc4e6aa
Peter Goodspeed-Niklaus <peter.goodspeed-niklaus@wire.com>
2026-02-24 14:33:56 +0100

    fix: make encryption detection work

--- ffi/src/database/is_encrypted.rs
+++ ffi/src/database/is_encrypted.rs
@@ -1,4 +1,3 @@
-use futures_lite::AsyncReadExt;
 use wasm_bindgen::prelude::*;
 
 use crate::{Context as _, Database, Result};
@@ -13,24 +12,12 @@ impl Database {
     /// Returns `Ok(true)` if encrypted, `Ok(false)` if unencrypted.
     pub async fn is_encrypted(&self) -> Result<bool> {
         // SQLite magic header for unencrypted databases
-        const SQLITE_MAGIC: [u8; 16] = *b"SQLite format 3\0";
+        const SQLITE_MAGIC: &[u8; 16] = b"SQLite format 3\0";
 
-        if !tokio_fs_ext::try_exists(&self.name)
-            .await
-            .context("checking database file existence")?
-        {
-            // nonexistint database is not encrypted and is not an error
-            return Ok(false);
-        }
+        let data = self
+            .export()
+            .context("exporting database to check header")?;
 
-        let mut buffer = [0; 16];
-        let mut file = tokio_fs_ext::File::open(&self.name)
-            .await
-            .context("opening database file")?;
-        file.read_exact(&mut buffer)
-            .await
-            .context("reading first 16 bytes from opfs")?;
-
-        Ok(buffer != SQLITE_MAGIC)
+        Ok(!data.starts_with(SQLITE_MAGIC))
     }
 }
